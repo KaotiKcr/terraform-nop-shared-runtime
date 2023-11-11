57c4b603658ec35b0e99f2048a89a646be1b1a7f
Ivan Castillo <ivan.castillo@triquimas.cr>
2023-11-10 20:39:29 -0600

    Init

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # terraform-nop-shared-runtime
+
+shared runtime
+    Data - MySQL + PostgreSQL
+    Storage - for graphics and images
+    Networking
+        app private
+        app public
+        data
+    DNS
+
+runtime
+    nop-demo.kaotik.cr
+    perfumes.cr
+    tienda.euro-confort.com
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  description = "The AWS region in which resources are set up."
+  type        = string
+  default     = "us-east-1"
+}
+
+
+variable "vpc_id" {
+  description = "Existing VPC to use (specify this, if you don't want to create new VPC)"
+  type        = string
+  default     = ""
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+  default     = "0.0.0.0/0"
+}
