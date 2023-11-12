f25bf671af762ee61ee71cb6b1b8d119125b7edd
Ivan Castillo <ivan.castillo@triquimas.cr>
2023-11-11 22:24:04 -0600

    Shared EC2

--- outputs.tf
+++ outputs.tf
@@ -3,10 +3,17 @@ output "hello_kaotik" {
 }
 
 output "vpc_id" {
-  description = "ID of the VPC"
   value       = data.aws_vpc.selected.id
 }
 
 output "aws_instance_public_dns" {
   value = aws_instance.shared_kaotik.public_dns
+}
+
+output "aws_instance_private_ip" {
+  value = "${aws_instance.shared_kaotik.private_ip}"
+}
+
+output "aws_instance_public_elastic_ip" {
+  value = "${aws_eip.shared_kaotik.public_ip}"
 }
