62104a41bfb6b062b16360e83f895b350eaf50b6
Ivan Castillo <ivan.castillo@triquimas.cr>
2023-11-12 22:15:25 -0600

    Shared EC2

--- ec2.tf
+++ ec2.tf
@@ -28,7 +28,10 @@ resource "aws_instance" "webserver" {
 
   vpc_security_group_ids = [aws_security_group.internet-access.id]
   aws_availability_zones = element(local.azs, 0)
-  subnet_id              = element(data.aws_subnets.default, 0).id
+  subnet_id              = element(data.aws_subnets.all, 0).id
+  #subnet_id              = sort(data.aws_subnets.all.ids)[0].id
+  #subnet_id              = data.aws_subnets.all[0].id
+
 
   tags = merge(local.tags,{
         Name = "${local.name_prefix}-webserver"
